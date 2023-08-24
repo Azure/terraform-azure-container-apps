bdfcae94085537befe1b3eb97a611681cf03ae01
Sebastian Ickler <sebastian.ickler@bbraun.com>
2023-08-24 15:03:29 +0200

    default_domain, container_app_fqdn and container_app_uri to outputs
    * add default_domain to outputs to determine internal
      loadbalancer IP for Private Link Service integration
    
    * rename container_app_fqdn to container_app_uri and make
      container_app_fqdn a real FQDN
      The actual fqdn is in fact a URI, which can't be used as
      Front Door Origin, as it requires a bare FQDN.

--- outputs.tf
+++ outputs.tf
@@ -5,10 +5,20 @@ output "container_app_environment_id" {
 
 output "container_app_fqdn" {
   description = "The FQDN of the Container App's ingress."
+  value       = { for name, container in azurerm_container_app.container_app : name => "${try(container.ingress[0].fqdn, "")}" if can(container.ingress[0].fqdn) }
+}
+
+output "container_app_uri" {
+  description = "The URI of the Container App's ingress."
   value       = { for name, container in azurerm_container_app.container_app : name => "https://${try(container.ingress[0].fqdn, "")}" if can(container.ingress[0].fqdn) }
 }
 
 output "container_app_ips" {
   description = "The IPs of the Latest Revision of the Container App."
   value       = azurerm_container_app_environment.container_env.static_ip_address
+}
+
+output "default_domain" {
+  description = "The default domain of the Container App Environment."
+  value       = azurerm_container_app_environment.container_env.default_domain
 }
