d2256362a2381de81ca62ab53750f1d1961cb9c0
David J. M. Karlsen <david.johan.macdonald.karlsen@dnb.no>
2023-12-04 00:30:46 +0100

    add provider

--- examples/existing-env/versions.tf
+++ examples/existing-env/versions.tf
@@ -2,6 +2,10 @@ terraform {
   required_version = ">= 1.2"
 
   required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 1.10.0"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.11, < 4.0"
