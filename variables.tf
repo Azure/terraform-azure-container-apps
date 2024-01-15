0c6919602c08d66f053bfca3fb3b0f3c7a886681
zjhe <lonegunmanb@hotmail.com>
2024-01-15 16:41:57 +0800

    fix format issue

--- variables.tf
+++ variables.tf
@@ -143,7 +143,7 @@ variable "container_apps" {
     error_message = "At least one container should be provided."
   }
   validation {
-    condition = alltrue([for n, c in var.container_apps : c.ingress == null ? true : (c.ingress.ip_security_restrictions == null ? true : (length(distinct([for r in c.ingress.ip_security_restrictions : r.action])) <= 1))])
+    condition     = alltrue([for n, c in var.container_apps : c.ingress == null ? true : (c.ingress.ip_security_restrictions == null ? true : (length(distinct([for r in c.ingress.ip_security_restrictions : r.action])) <= 1))])
     error_message = "The `action` types in an all `ip_security_restriction` blocks must be the same for the `ingress`, mixing `Allow` and `Deny` rules is not currently supported by the service."
   }
 }
