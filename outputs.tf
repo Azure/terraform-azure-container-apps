a4104e1581090465b30751f9c2d191fc612825b0
Sebastian Ickler <91525188+icklsede@users.noreply.github.com>
2024-11-17 20:03:45 -0800

    Better Front Door and Private Link integration due to module outputs (#20)
    * default_domain, container_app_fqdn and container_app_uri to outputs
    
    * add default_domain to outputs to determine internal
      loadbalancer IP for Private Link Service integration
    
    * rename container_app_fqdn to container_app_uri and make
      container_app_fqdn a real FQDN
      The actual fqdn is in fact a URI, which can't be used as
      Front Door Origin, as it requires a bare FQDN.
    
    * Update outputs.tf
    
    * Update versions.tf
    
    ---------
    
    Co-authored-by: lonegunmanb <lonegunmanb@hotmail.com>

--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,11 @@ output "container_app_environment_id" {
 
 output "container_app_fqdn" {
   description = "The FQDN of the Container App's ingress."
+  value       = { for name, container in azurerm_container_app.container_app : name => "${try(container.ingress[0].fqdn, "")}" if can(container.ingress[0].fqdn) }
+}
+
+output "container_app_uri" {
+  description = "The URI of the Container App's ingress."
   value       = { for name, container in azurerm_container_app.container_app : name => "https://${try(container.ingress[0].fqdn, "")}" if can(container.ingress[0].fqdn) }
 }
 
@@ -21,4 +26,9 @@ output "container_app_identities" {
 output "container_app_ips" {
   description = "The IPs of the Latest Revision of the Container App."
   value       = try(azurerm_container_app_environment.container_env[0].static_ip_address, data.azurerm_container_app_environment.container_env[0].static_ip_address)
+}
+
+output "default_domain" {
+  description = "The default domain of the Container App Environment."
+  value       = azurerm_container_app_environment.container_env.default_domain
 }
